d14ba8da6cfad39cbb7c362a444630866513fb49
Lokin <lokin.makkena@colorado.edu>
2020-04-10 23:44:36 -0600

    Returns three different losses

--- GAN_model2.py
+++ GAN_model2.py
@@ -84,7 +84,7 @@ def discriminator_loss(cross_entropy, real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
-    return total_loss
+    return total_loss, real_loss, fake_loss
 
 
 def generator_loss(cross_entropy, fake_output):
@@ -105,7 +105,7 @@ def train_step(images, generator, discriminator, \
         real_output = discriminator(images, training=True)
         fake_output = discriminator(generated_images, training=True)
         gen_loss = generator_loss(cross_entropy, fake_output)
-        disc_loss = discriminator_loss(cross_entropy, real_output, fake_output)
+        disc_loss, disc_real_loss, disc_fake_loss = discriminator_loss(cross_entropy, real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
@@ -113,7 +113,7 @@ def train_step(images, generator, discriminator, \
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
-    return disc_loss, gen_loss
+    return disc_real_loss, disc_fake_loss, gen_loss
 
 def train(dataset, epochs, batch_size, noise_dim, checkpoint_dir='./training_checkpoints'):
     generator = make_generator_model(noise_dim)
@@ -127,20 +127,22 @@ def train(dataset, epochs, batch_size, noise_dim, checkpoint_dir='./training_che
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
     image_helper = Image_helper(noise_dim)
 
-    avg_desc_loss, avg_gen_loss = [], []
+    avg_disc_real_loss, avg_disc_fake_loss, avg_gen_loss = [], [], []
 
     for epoch in range(epochs):
         start = time.time()
         for image_batch in dataset:
-            d_loss_vals, g_loss_vals = [],[]
+            d_real_loss_vals, d_fake_loss_vals, g_loss_vals = [], [], []
 
-            d_loss, g_loss = train_step(image_batch, generator, discriminator,
+            disc_real_loss, disc_fake_loss, g_loss = train_step(image_batch, generator, discriminator,
                        generator_optimizer, discriminator_optimizer, cross_entropy, batch_size)
 
-            d_loss_vals.append(float(d_loss))
+            d_real_loss_vals.append(float(disc_real_loss))
+            d_fake_loss_vals.append(float(disc_fake_loss))
             g_loss_vals.append(float(g_loss))
 
-        avg_desc_loss.append(np.mean(d_loss_vals))
+        avg_disc_real_loss.append(np.mean(d_real_loss_vals))
+        avg_disc_fake_loss.append(np.mean(d_fake_loss_vals))
         avg_gen_loss.append(np.mean(g_loss_vals))
 
         if (epoch + 1) % 5 == 0:
@@ -148,14 +150,15 @@ def train(dataset, epochs, batch_size, noise_dim, checkpoint_dir='./training_che
             image_helper.generate_and_save_images(generator, epoch + 1)
 
         print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
-        print("Epoch " + str(epoch + 1) + "- Discriminator Loss:", np.mean(avg_desc_loss), "Generator Loss:",
-              np.mean(avg_gen_loss))
+        print("Epoch " + str(epoch + 1) + "- Real Discriminator Loss:", np.mean(avg_disc_real_loss),
+              "Fake Discriminator Loss:", np.mean(avg_disc_fake_loss),
+              "Generator Loss:", np.mean(avg_gen_loss))
         print("\n")
-        
+
     # Generate after the final epoch
     image_helper.generate_and_save_images(generator, epochs)
 
-    return avg_desc_loss, avg_gen_loss
+    return avg_disc_real_loss, avg_disc_fake_loss, avg_gen_loss
 
 # def image_to_dataset(images, batch_size, buffer_size):
 #     dataset = tf.data.Dataset.from_tensor_slices(images) \
@@ -165,18 +168,19 @@ def train(dataset, epochs, batch_size, noise_dim, checkpoint_dir='./training_che
 if __name__ == '__main__':
     buffer_size = 6000
     batch_size = 10
-    epochs = 10
+    epochs = 5
     noise_dim = 100
     # images = get_images()
     # train_dataset = image_to_dataset(images, batch_size, buffer_size)
     train_dataset = get_h5_images()
     train_dataset = train_dataset.shuffle(buffer_size).batch(batch_size)
     # train_dataset = get_celeb_dataset(batch_size, buffer_size)
-    desc_loss, gen_loss = train(train_dataset, epochs, batch_size, noise_dim)
+    real_desc_loss, fake_desc_loss, gen_loss = train(train_dataset, epochs, batch_size, noise_dim)
 
     fig = plt.figure(figsize=(12,8))
     plt.plot(range(epochs), gen_loss, "-b", label='generator loss')
-    plt.plot(range(epochs), desc_loss, "-r", label='discriminator loss')
+    plt.plot(range(epochs), real_desc_loss, "-r", label='real discriminator loss')
+    plt.plot(range(epochs), fake_desc_loss, "-y", label='fake discriminator loss')
     plt.legend(loc="upper left")
     plt.xlabel("epochs")
     plt.ylabel("loss")
