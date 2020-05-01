ec0a1410eb355f38cf0ce2bd4ac540bfbaa423f9
andyfan <duanwei.fan@colorado.edu>
2020-05-01 15:17:49 -0600

    set param for infoGAN, batch_size=16, n_control_cat=8

--- trainer.py
+++ trainer.py
@@ -1,8 +1,7 @@
-from GAN_model import GAN
 from InfoGAN_model import InfoGAN
 
 def run():
-    gan = InfoGAN(batch_size=32, epochs=10, filename='cartoon_dataset.h5', key='cartoon')
+    gan = InfoGAN(batch_size=16, epochs=1000, n_control_cat=8, filename='cartoon_dataset.h5', key='cartoon')
     gan.train()
 
 if __name__ == '__main__':
